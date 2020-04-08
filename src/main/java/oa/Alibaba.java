b07c965249efdfeef31f13bfa31d4f212040b8fc
Shenyi Lu <30668724+ShenyiLu@users.noreply.github.com>
2020-04-08 02:08:40 -0700

    alibaba OA epic fail

--- src/main/java/oa/Alibaba.java
+++ src/main/java/oa/Alibaba.java
@@ -1,4 +1,39 @@
 package oa;
 
+import java.util.Scanner;
+
 public class Alibaba {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int[] nmab = new int[4];
+            for (int j = 0; j < nmab.length; j++) {
+                nmab[j] = in.nextInt();
+            }
+            System.out.println(training(nmab));
+        }
+    }
+
+    private static int training(int[] nmab) {
+        int n = nmab[0];
+        int m = nmab[1];
+        int a = nmab[2];
+        int b = nmab[3];
+        if (a > n) {
+            return 0;
+        }
+        if (a == n) {
+            return Math.min(m, b);
+        }
+
+        if (m <= b) {
+            return m;
+        }
+
+        int maxPossible = b + m % b;
+
+        return Math.min(m, (n * b) / a);
+    }
 }
---
+++ src/main/java/oa/Alibaba2.java
@@ -0,0 +1,27 @@
+package oa;
+
+import java.util.Scanner;
+
+public class Alibaba2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            int[] nmab = new int[4];
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = in.nextInt();
+            }
+
+        }
+
+
+    }
+
+    private static int step() {
+        return 0;
+    }
+}
